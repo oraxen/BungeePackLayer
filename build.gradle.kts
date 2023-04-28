41eea2362e4b579d7f610ed7f9a60e64439df594
Boy <sivertpaulsen2@gmail.com>
2023-04-28 16:52:13 +0200

    change to gradle kts syntax

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+}
+
+val pluginVersion: String by project
+val pluginDescription = "Optimize texture pack management on bungee/velocity to never double send the same pack."
+group = "io.th0rgal.packsmanager"
+version = pluginVersion
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://mvn.exceptionflug.de/repository/exceptionflug-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("dev.simplix:protocolize-api:2.2.6")
+    compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-proxy:3.0.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+}
+
+
+
+tasks {
+    jar {
+        filesMatching(arrayOf("bungee.yml", "velocity-plugin.json").asIterable()) {
+            expand(mapOf("version" to pluginVersion, "description" to pluginDescription))
+        }
+    }
+
+    compileJava.get().dependsOn(clean)
+}
